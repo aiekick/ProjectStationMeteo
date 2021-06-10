c5d38b989c76e0de6dfc40db069494ea0abc0a16
aiekick <aiekick@funparadigm.com>
2021-06-10 19:42:27 +0200

    [ADD] : add a makefile and fix issue in main.cpp (was not listening)

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,14 @@
 int main(int argc, char** argv)
 {
     uv::EventLoop* loop = uv::EventLoop::DefaultLoop();
-    uv::TcpServer::SetBufferMode(uv::GlobalConfig::BufferMode::CycleBuffer);
-    uv::SocketAddr addr("127.0.0.1", 12001);
-    uv::TcpServer server(loop);
-
+    uv::SocketAddr addr("192.168.0.62", 12001, uv::SocketAddr::Ipv4);
+    
+	uv::TcpServer server(loop);
     server.setMessageCallback([](uv::TcpConnectionPtr ptr, const char* data, ssize_t size)
         {
             printf("Msg Received : %s\n", data);
         });
 
+	server.bindAndListen(addr);
     loop->run();
 }
