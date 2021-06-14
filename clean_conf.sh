30ed6ca20c2f58c28830955d8ddd32291d18eefe
aiekick <aiekick@funparadigm.com>
2021-06-14 12:47:09 +0200

    [RMV] : remove the cmake way and lib src doe [ADD] : add script for create makefile and configure with autotools         the configure need the libs libuv1-dev and libsqlite3-dev, there is an error in configure if missing [ADD] : add bme280 and uv-cpp libs code

---
+++ clean_conf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm *.o 
+rm *.m4 
+rm *.log 
+rm *.tar.gz 
+rm *.sub
+rm *.status
+rm *.guess
+rm install-sh
+rm bme280Server.service
+rm compile
+rm depcomp 
+rm missing
+rm configure
+rm Makefile 
+rm Makefile.in
+rm -R autom4te.cache 
+rm -R .deps 
+rm -R bme280server-0.1
+
+
