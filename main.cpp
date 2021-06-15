5e6530d7fd86f1c5bfd1b961d96cff34f1cc7330
ANTHEAUME Mickael <Mickael.ANTHEAUME@akka.eu>
2021-06-15 10:22:34 +0200

    -

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "infosClient.hpp"
+#include "balise.h"
+#include "balisevillepanel.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+   // MainWindow w;
+    //w.show();
+
+    balisevillepanel b;
     return a.exec();
 }
