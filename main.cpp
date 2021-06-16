2e43be972c33e97b669d63cea80f498a49592bed
aiekick <aiekick@funparadigm.com>
2021-06-16 17:32:43 +0200

    [FIX]  fix : fix translation main

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    MainWindow w;
-    w.show();
 
     QTranslator myappTranslator;
     myappTranslator.load(QLocale(), QLatin1String("myapp"), QLatin1String("_"), QLatin1String(":/i18n"));
     app.installTranslator(&myappTranslator);
+
+    MainWindow w;
+    w.show();
 
     return app.exec();
 }
