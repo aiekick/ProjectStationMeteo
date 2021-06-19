d9fd31d4a71c6dc5c6ebccb3cefcb7299af0cfbc
ls-resk <80887120+ls-resk@users.noreply.github.com>
2021-06-19 13:09:45 +0200

    [ADD]: Add graph for each parameter to the UI

--- baliseMer.h
+++ baliseMer.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <QPixmap>
 #include <QtCharts>
+#include <QSplineSeries>
+#include <qlineseries.h>
 
 class BaliseMer : public Balise
 {
@@ -22,6 +24,9 @@ public:
 	DatasMeteo getSummary();
 	QPixmap displayWeatherIcon();
 	QChartView* displayDetailedChart();
+	QChartView* humidityChart();
+	QChartView* pressureChart();
+	QChartView* temperatureChart();
 
 	void requestData();     // Send request to server and retrieve infos
 	void requestMeanData(); // Same as requestData but only for temperature over the last 12 hours
